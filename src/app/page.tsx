e99ba8a4eecf81815c390bac5f2838980f414a37
joaodlouro <joaovitorvx1315@gmail.com>
2025-08-23 19:23:35 -0300

    page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,27 @@
 import BpmnEditor from "./Components/BpmnEditor";
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="w-full max-w-5xl flex flex-col items-center gap-6">
-     
+    <div
+      className="fixed inset-0 flex flex-col items-center gap-6"
+      style={{
+        backgroundImage: "url('/city.webp')",
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+      }}
+    >
       <BpmnEditor />
+
+
+      <Image
+        className="absolute bottom-0 left-0  translate-y-[-5px]"
+        src="/dark.png"
+        alt="soldado"
+        width={400}
+        height={500}
+      />
     </div>
   );
 }
